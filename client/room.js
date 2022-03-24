572914da876ca6b229c0fe5d7be8eb9ed281a498
Sahajpreet Singh <sp.code2003@gmail.com>
2022-03-24 22:04:50 +0530

    Final commit

--- client/room.js
+++ client/room.js
@@ -1,77 +1,85 @@
-var par = JSON.parse(localStorage.getItem("part"));
-var scores = [];
-for(var i=0;i<par.length;i++){
-    scores[i]=0;
-}
-var idx;
-var username = localStorage.getItem("username");
-const writeEvent = (p) =>{
+
+const id = localStorage.getItem("id");
+console.log("id = " + id)
+var par = JSON.parse(localStorage.getItem("part" + id));
+console.log(par)
+var scores = JSON.parse(localStorage.getItem("scores" + id));
+console.log(scores)
+
+var yo = 0;
+var idx = localStorage.getItem("idx" + id);
+var username = localStorage.getItem("naam");
+console.log("username = " + username)
+const writeEvent = (p) => {
     const parent = document.querySelector('#participants');
-    parent.innerHTML="";
+    parent.innerHTML = "";
     console.log(p);
-    for(var j=0;j<p.length;j++){
+    for (var j = 0; j < p.length; j++) {
         const el = document.createElement('li');
-        el.innerHTML = p[j]+"==========> "+scores[j];
+        el.innerHTML = p[j];
         console.log(p[j]);
         parent.appendChild(el);
-        if(p[j]==username){
-            idx=j;
-        }
     }
 };
 console.log(par);
-const onFormSubmitted = (e) =>{
+const onFormSubmitted = (e) => {
     e.preventDefault();
-    sock.emit('message', par);  
+    sock.emit('message' + id, par);
+    document.getElementById('gif').src = './images/Music.gif';
 };
-
-
-const sock =io();
-sock.on('message', writeEvent);
-document.querySelector('#yo').addEventListener('submit',onFormSubmitted);
+const sock = io();
+document.querySelector('#yo').addEventListener('submit', onFormSubmitted);
 var timeleft;
-function do_change1(){
+function do_change1() {
     document.getElementById("start").hidden = true;
-    timeleft=10;
-    var Timer = setInterval(function(){
-    if(timeleft <= 0){
-      document.getElementById("yo").style.display = "block";
-      document.getElementById("countdown").innerHTML = "Finished";
-      pause();
-      clearInterval(Timer);
-      document.getElementById("next").hidden= false;
-    } else {
-      document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-    }
-    timeleft -= 1;
-  }, 1000);
+    timeleft = 30;
+    var Timer = setInterval(function () {
+        if (timeleft <= 0) {
+            document.getElementById("yo").style.display = "block";
+            document.getElementById("countdown").innerHTML = "Finished";
+            document.getElementById('gif').src = './images/Music.png';
+            pause();
+            clearInterval(Timer);
+            document.getElementById("next").hidden = false;
+            document.getElementById("chat").readOnly = true;
+        } else {
+            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+            document.getElementById("chat").readOnly = false;
+            document.getElementById('gif').src = './images/Music.gif';
+        }
+        timeleft -= 1;
+    }, 1000);
     play();
 };
-function do_change2(){
-    document.getElementById("next").hidden= true;
-    timeleft=10;
-    Timer=setInterval(function(){
-        if(timeleft <= 0){
-        document.getElementById("yo").style.display = "block";
-        document.getElementById("countdown").innerHTML = "Finished";
-        pause();
-        clearInterval(Timer);
-        document.getElementById("next").hidden= false;
-      } else {
-        document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-      }
-      timeleft -= 1;
-    },1000);
+function do_change2() {
+
+    document.getElementById("next").hidden = true;
+    timeleft = 30;
+    Timer = setInterval(function () {
+        if (timeleft <= 0) {
+            document.getElementById("yo").style.display = "block";
+            document.getElementById("countdown").innerHTML = "Finished";
+            pause();
+            clearInterval(Timer);
+            document.getElementById("next").hidden = false;
+            document.getElementById('gif').src = './images/Music.png';
+            document.getElementById("chat").readOnly = true;
+        } else {
+            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+            document.getElementById("chat").readOnly = false;
+            document.getElementById('gif').src = './images/Music.gif';
+        }
+        timeleft -= 1;
+    }, 1000);
     next();
 };
 
 
 var redirect_uri = "http://localhost:8080/room.html";
- 
 
-var client_id = "bde4679897144e169aa76462ac24e784"; 
-var client_secret = "607cd05baa764effae70e1f10a5fe7d7"; // In a real app you should not expose your client_secret to the user
 
+var client_id = "347a92cb28cd416c9d74727716d0bc19";
+var client_secret = "4418c6bbe4e84443aa55658242bc77d6";
 var access_token = null;
 var refresh_token = null;
 var currentPlaylist = "";
@@ -88,42 +96,42 @@ const TRACKS = "https://api.spotify.com/v1/playlists/7e0FCrrogClFLW8yQ1T8kF/trac
 const CURRENTLYPLAYING = "https://api.spotify.com/v1/me/player/currently-playing";
 const SHUFFLE = "https://api.spotify.com/v1/me/player/shuffle";
 
-function onPageLoad(){
-    if ( window.location.search.length > 0 ){
+function onPageLoad() {
+    if (window.location.search.length > 0) {
         handleRedirect();
     }
-    else{
+    else {
         access_token = localStorage.getItem("access_token");
-        if ( access_token == null ){
+        if (access_token == null) {
             // we don't have an access token so present token section
-            document.getElementById("tokenSection").style.display = 'block';  
+            document.getElementById("tokenSection").style.display = 'block';
         }
         else {
             // we have an access token so present device section
-            document.getElementById("deviceSection").style.display = 'block';  
+            document.getElementById("deviceSection").style.display = 'block';
             refreshDevices();
             currentlyPlaying();
         }
     }
 }
 
-function handleRedirect(){
+function handleRedirect() {
     let code = getCode();
-    fetchAccessToken( code );
+    fetchAccessToken(code);
     window.history.pushState("", "", redirect_uri); // remove param from url
 }
 
-function getCode(){
+function getCode() {
     let code = null;
     const queryString = window.location.search;
-    if ( queryString.length > 0 ){
+    if (queryString.length > 0) {
         const urlParams = new URLSearchParams(queryString);
         code = urlParams.get('code')
     }
     return code;
 }
 
-function requestAuthorization(){
+function requestAuthorization() {
     let url = AUTHORIZE;
     url += "?client_id=" + client_id;
     url += "&response_type=code";
@@ -133,16 +141,16 @@ function requestAuthorization(){
     window.location.href = url; // Show Spotify's authorization screen
 }
 
-function fetchAccessToken( code ){
+function fetchAccessToken(code) {
     let body = "grant_type=authorization_code";
-    body += "&code=" + code; 
+    body += "&code=" + code;
     body += "&redirect_uri=" + encodeURI(redirect_uri);
     body += "&client_id=" + client_id;
     body += "&client_secret=" + client_secret;
     callAuthorizationApi(body);
 }
 
-function refreshAccessToken(){
+function refreshAccessToken() {
     refresh_token = localStorage.getItem("refresh_token");
     let body = "grant_type=refresh_token";
     body += "&refresh_token=" + refresh_token;
@@ -150,25 +158,25 @@ function refreshAccessToken(){
     callAuthorizationApi(body);
 }
 
-function callAuthorizationApi(body){
+function callAuthorizationApi(body) {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", TOKEN, true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+    xhr.setRequestHeader('Authorization', 'Basic ' + 'MzQ3YTkyY2IyOGNkNDE2YzlkNzQ3Mjc3MTZkMGJjMTk6NDQxOGM2YmJlNGU4NDQ0M2FhNTU2NTgyNDJiYzc3ZDY=');
     xhr.send(body);
     xhr.onload = handleAuthorizationResponse;
 }
 
-function handleAuthorizationResponse(){
-    if ( this.status == 200 ){
+function handleAuthorizationResponse() {
+    if (this.status == 200) {
         var data = JSON.parse(this.responseText);
         console.log(data);
         var data = JSON.parse(this.responseText);
-        if ( data.access_token != undefined ){
+        if (data.access_token != undefined) {
             access_token = data.access_token;
             localStorage.setItem("access_token", access_token);
         }
-        if ( data.refresh_token  != undefined ){
+        if (data.refresh_token != undefined) {
             refresh_token = data.refresh_token;
             localStorage.setItem("refresh_token", refresh_token);
         }
@@ -180,18 +188,18 @@ function handleAuthorizationResponse(){
     }
 }
 
-function refreshDevices(){
-    callApi( "GET", DEVICES, null, handleDevicesResponse );
+function refreshDevices() {
+    callApi("GET", DEVICES, null, handleDevicesResponse);
 }
 
-function handleDevicesResponse(){
-    if ( this.status == 200 ){
+function handleDevicesResponse() {
+    if (this.status == 200) {
         var data = JSON.parse(this.responseText);
         console.log(data);
-        removeAllItems( "devices" );
+        removeAllItems("devices");
         data.devices.forEach(item => addDevice(item));
     }
-    else if ( this.status == 401 ){
+    else if (this.status == 401) {
         refreshAccessToken()
     }
     else {
@@ -200,14 +208,14 @@ function handleDevicesResponse(){
     }
 }
 
-function addDevice(item){
+function addDevice(item) {
     let node = document.createElement("option");
     node.value = item.id;
     node.innerHTML = item.name;
-    document.getElementById("devices").appendChild(node); 
+    document.getElementById("devices").appendChild(node);
 }
 
-function callApi(method, url, body, callback){
+function callApi(method, url, body, callback) {
     let xhr = new XMLHttpRequest();
     xhr.open(method, url, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
@@ -216,129 +224,128 @@ function callApi(method, url, body, callback){
     xhr.onload = callback;
 }
 
-function removeAllItems( elementId ){
+function removeAllItems(elementId) {
     let node = document.getElementById(elementId);
     while (node.firstChild) {
         node.removeChild(node.firstChild);
     }
 }
 
-function play(){
+function play() {
     let playlist_id = "7e0FCrrogClFLW8yQ1T8kF";
     let trackindex = document.getElementById("tracks").value;
     let album = document.getElementById("album").value;
     let body = {};
-    if ( album.length > 0 ){
+    if (album.length > 0) {
         body.context_uri = album;
     }
-    else{
+    else {
         body.context_uri = "spotify:playlist:" + playlist_id;
     }
     body.offset = {};
     body.offset.position = trackindex.length > 0 ? Number(trackindex) : 0;
     body.offset.position_ms = 0;
-    callApi( "PUT", PLAY + "?device_id=" + deviceId(), JSON.stringify(body), handleApiResponse );
+    callApi("PUT", PLAY + "?device_id=" + deviceId(), JSON.stringify(body), handleApiResponse);
 }
 
-function shuffle(){
-    callApi( "PUT", SHUFFLE + "?state=true&device_id=" + deviceId(), null, handleApiResponse );
-    play(); 
-}
-
-function pause(){
-    callApi( "PUT", PAUSE + "?device_id=" + deviceId(), null, handleApiResponse );
-}
-
-function next(){
-    callApi( "POST", NEXT + "?device_id=" + deviceId(), null, handleApiResponse );
-    
-}
-
-function previous(){
-    callApi( "POST", PREVIOUS + "?device_id=" + deviceId(), null, handleApiResponse );
-}
-
-
-function handleApiResponse(){
-    if ( this.status == 200){
-        console.log(this.responseText);
-        setTimeout(currentlyPlaying, 2000);
-    }
-    else if ( this.status == 204 ){
-        setTimeout(currentlyPlaying, 2000);
-    }
-    else if ( this.status == 401 ){
-        refreshAccessToken()
-    }
-    else {
-        console.log(this.responseText);
-        alert(this.responseText);
-    }    
-}
-
-function deviceId(){
-    return document.getElementById("devices").value;
-}
-
-function fetchTracks(){
-        callApi( "GET", TRACKS, null, handleTracksResponse );
-}
-
-function handleTracksResponse(){
-    if ( this.status == 200 ){
-        var data = JSON.parse(this.responseText);
-        console.log(data);
-        removeAllItems( "tracks" );
-        data.items.forEach( (item, index) => addTrack(item, index));
-    }
-    else if ( this.status == 401 ){
-        refreshAccessToken()
-    }
-    else {
-        console.log(this.responseText);
-        alert(this.responseText);
-    }
-    
-}
-
-function addTrack(item, index){
-    let node = document.createElement("option");
-    node.value = index;
-    node.innerHTML = item.track.name + " (" + item.track.artists[0].name + ")";
-    document.getElementById("tracks").appendChild(node); 
+function shuffle() {
+    callApi("PUT", SHUFFLE + "?state=true&device_id=" + deviceId(), null, handleApiResponse);
     play();
 }
 
-function currentlyPlaying(){
-    callApi( "GET", PLAYER + "?market=US", null, handleCurrentlyPlayingResponse );
+function pause() {
+    callApi("PUT", PAUSE + "?device_id=" + deviceId(), null, handleApiResponse);
 }
 
-function handleCurrentlyPlayingResponse(){
-    if ( this.status == 200 ){
+function next() {
+    callApi("POST", NEXT + "?device_id=" + deviceId(), null, handleApiResponse);
+
+}
+
+function previous() {
+    callApi("POST", PREVIOUS + "?device_id=" + deviceId(), null, handleApiResponse);
+}
+
+
+function handleApiResponse() {
+    if (this.status == 200) {
+        console.log(this.responseText);
+        setTimeout(currentlyPlaying, 2000);
+    }
+    else if (this.status == 204) {
+        setTimeout(currentlyPlaying, 2000);
+    }
+    else if (this.status == 401) {
+        refreshAccessToken()
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
+function deviceId() {
+    return document.getElementById("devices").value;
+}
+
+function fetchTracks() {
+    callApi("GET", TRACKS, null, handleTracksResponse);
+}
+
+function handleTracksResponse() {
+    if (this.status == 200) {
         var data = JSON.parse(this.responseText);
         console.log(data);
-        if ( data.item != null ){
-            document.getElementById("albumImage").src = data.item.album.images[0].url;
+        removeAllItems("tracks");
+        data.items.forEach((item, index) => addTrack(item, index));
+    }
+    else if (this.status == 401) {
+        refreshAccessToken()
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+
+}
+
+function addTrack(item, index) {
+    let node = document.createElement("option");
+    node.value = index;
+    node.innerHTML = item.track.name + " (" + item.track.artists[0].name + ")";
+    document.getElementById("tracks").appendChild(node);
+    play();
+}
+
+function currentlyPlaying() {
+    callApi("GET", PLAYER + "?market=US", null, handleCurrentlyPlayingResponse);
+}
+
+function handleCurrentlyPlayingResponse() {
+    if (this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        console.log(data);
+        if (data.item != null) {
             document.getElementById("trackTitle").innerHTML = data.item.name;
             document.getElementById("trackArtist").innerHTML = data.item.artists[0].name;
         }
 
 
-        if ( data.device != null ){
+        if (data.device != null) {
             // select device
             currentDevice = data.device.id;
-            document.getElementById('devices').value=currentDevice;
+            document.getElementById('devices').value = currentDevice;
         }
 
-        if ( data.context != null ){
+        if (data.context != null) {
             currentPlaylist = data.context.uri;
-            currentPlaylist = currentPlaylist.substring( currentPlaylist.lastIndexOf(":") + 1,  currentPlaylist.length );
+            currentPlaylist = currentPlaylist.substring(currentPlaylist.lastIndexOf(":") + 1, currentPlaylist.length);
         }
     }
-    else if ( this.status == 204 ){
+    else if (this.status == 204) {
 
     }
-    else if ( this.status == 401 ){
+    else if (this.status == 401) {
         refreshAccessToken()
     }
     else {
@@ -351,24 +358,50 @@ const writeAns = (text) => {
     const parent = document.querySelector('#events');
     const el = document.createElement('li');
     el.innerHTML = text;
-  
     parent.appendChild(el);
-  };
-  
-  const Answered = (e) => {
+
+};
+
+const Answered = (e) => {
     e.preventDefault();
-  
     const input = document.querySelector('#chat');
     const text = input.value;
-    input.value = '';
-  
-    sock2.emit('message', text);
-  };
-  const sock2 = io();
-  sock2.on('message', writeAns);
-  
-  document
+    if (text == document.getElementById("trackTitle").innerHTML) {
+        var t = scores[idx];
+        t = parseInt(t) + 100;
+        scores[idx] = t;
+        localStorage.setItem("scores" + id, JSON.stringify(scores));
+        par[idx] = username + " has scored " + scores[idx];
+        localStorage.setItem("part" + id, JSON.stringify(par));
+        alert("You got it right!!!");
+        input.value = '';
+        sock.emit('chat' + id, username + " got it right!!! ");
+        sock.emit('message' + id, par);
+        document.getElementById("chat").readOnly = true;
+    } else {
+        input.value = '';
+        sock.emit('chat' + id, username + ": " + text);
+    }
+};
+
+sock.on('chat' + id, writeAns);
+sock.on('message' + id, writeEvent);
+document
     .querySelector('#chat-form')
     .addEventListener('submit', Answered);
-  
-  
+
+
+const exit = (e) => {
+    e.preventDefault();
+    console.log(idx - 1)
+    par.splice(idx, 1);
+    scores.splice(idx, 1);
+    localStorage.setItem("part" + id, JSON.stringify(par));
+    localStorage.setItem("scores" + id, JSON.stringify(scores));
+    sock.emit('chat' + id, username + " left ");
+    sock.emit('message' + id, par);
+    window.close();
+};
+document
+    .querySelector('#tl')
+    .addEventListener('submit', exit);
